4e03ce87182285f33a4265a31c988f728d9b193d
T190027 <T190027@mail.ryukoku.ac.jp>
2020-07-20 02:56:53 +0000

    提出

--- kadai11-1.c
+++ kadai11-1.c
@@ -2,22 +2,22 @@
 #include <math.h>
 
 
-double matukidairahagetane(double x){
+double hanapittedare(double x){
     return (4/(1+x*x));
 }
 
-double matukidairaseiritekinimuri(double a,double b,int n){
+double osanaisanngannmenndekai(double a,double b,int n){
     double k,s=0;
     int i;
     k=(b-a)/(2*n);
     for(i=0;i<=2*n;i++){
-        if(i==0||i==2*n) s=matukidairahagetane(a+i*k)+s;
-        else if(i%2==1) s=4*matukidairahagetane(a+i*k)+s;
-        else s=2*matukidairahagetane(a+i*k)+s;
+        if(i==0||i==2*n) s=hanapittedare(a,b,n)*(a+i*k)+s;
+        else if(i%2==1) s=4*hanapittedare(a+i*k)+s;
+        else s=2*hanapittedare(a+i*k)+s;
     }
     s=(s*k)/3;
     return (s);
-    
+
 }
 
 
@@ -27,7 +27,7 @@ int main(){
     double a=0.0,b=1.0,h;
 
     for(n=1;n<=256;n=2*n){
-        printf("%.14f\n",matukidairaseiritekinimuri(a,b,n));
+        printf("%.14f\n",osanaisanngannmenndekai(a,b,n));
     }
 
     return 0;
