02e0c88f1b6995ddc0905b5ee856fa8581f7ae3b
T190027 <T190027@mail.ryukoku.ac.jp>
2020-07-20 02:49:21 +0000

    提出

--- kadai11-1.c
+++ kadai11-1.c
@@ -2,21 +2,22 @@
 #include <math.h>
 
 
-double matukidairasine(double x){
+double matukidairahagetane(double x){
     return (4/(1+x*x));
 }
 
-double hayakusaitennsiro(double a,double b,int n){
+double matukidairaseiritekinimuri(double a,double b,int n){
     double k,s=0;
     int i;
     k=(b-a)/(2*n);
     for(i=0;i<=2*n;i++){
-        if(i==0||i==2*n) s=matukidairasine(a+i*k)+s;
-        else if(i%2==1) s=4*matukidairasine(a+i*k)+s;
-        else s=2*matukidairasine(a+i*k)+s;
+        if(i==0||i==2*n) s=matukidairahagetane(a+i*k)+s;
+        else if(i%2==1) s=4*matukidairahagetane(a+i*k)+s;
+        else s=2*matukidairahagetane(a+i*k)+s;
     }
     s=(s*k)/3;
     return (s);
+    
 }
 
 
@@ -26,7 +27,7 @@ int main(){
     double a=0.0,b=1.0,h;
 
     for(n=1;n<=256;n=2*n){
-        printf("%.14f\n",hayakusaitennsiro(a,b,n));
+        printf("%.14f\n",matukidairaseiritekinimuri(a,b,n));
     }
 
     return 0;
